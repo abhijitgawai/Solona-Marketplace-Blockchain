2b1b4a6336fce953d7c793608043e32f9a981cae
abhijitgawai <abhijit.gawai18@vit.edu>
2022-01-17 18:24:20 +0530

    2nd test runned chapter 2 section 2

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,14 +1,35 @@
+//<1>
+// use anchor_lang::prelude::*;
+
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");      // "program id" and has info for Solana on how to run our program. Anchor has generated this
+ 
+// #[program]
+// pub mod myepicproject {
+//     use super::*;
+//     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+//         Ok(())
+//     }
+// }
+
+// #[derive(Accounts)]
+// pub struct Initialize {}
+//</1>
+
+//<2>
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod myepicproject {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
+//</2>
+
+
--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -1,14 +1,44 @@
+
+//<1>
+// const anchor = require('@project-serum/anchor');
+
+// describe('myepicproject', () => {
+
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.Provider.env());
+
+//   it('Is initialized!', async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicproject;
+//     const tx = await program.rpc.initialize();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+//</1>
+
+//<2>
 const anchor = require('@project-serum/anchor');
 
-describe('myepicproject', () => {
+const main = async() => {
+  console.log("🚀 Starting test...")
 
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.Myepicproject;
+  const tx = await program.rpc.startStuffOff();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
+
+//</2>
