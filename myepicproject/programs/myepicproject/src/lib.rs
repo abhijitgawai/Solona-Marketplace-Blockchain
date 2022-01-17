2435454b756b31d611c2fa69258501ca0824f6a4
abhijitgawai <abhijit.gawai18@vit.edu>
2022-01-17 20:37:13 +0530

    deployed on devnet

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -38,7 +38,7 @@
 //<3>---------------------------------------------------
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GTxHy9wVzoPDPoMmSWiX74GPctQcKZvrfaW6L2ZCQeT7");
 
 #[program]
 pub mod myepicproject {
@@ -50,25 +50,63 @@ pub mod myepicproject {
     base_account.total_gifs = 0;
     Ok(())
   }
+
+	// Another function woo!
+  pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> ProgramResult {         // Context named AddGif that has access to a mutable reference to base_account
+    // Get a reference to the account and increment total_gifs.   // I can actually change the total_gifs value stored on BaseAccount
+    let base_account = &mut ctx.accounts.base_account;            // base_account which was passed in to the function via Context<AddGif>. Then, I increment the counter and that's it!!
+    let user = &mut ctx.accounts.user;
+
+    // Build the struct.
+    let item = ItemStruct {
+      gif_link: gif_link.to_string(),
+      user_address: *user.to_account_info().key,
+    };
+
+    // Add it to the gif_list vector.
+    base_account.gif_list.push(item);
+    base_account.total_gifs += 1;
+    Ok(())
+  }
 }
 
 // Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
 pub struct StartStuffOff<'info> {                                  // here we actually specify how to initialize it and what to hold in our StartStuffOff context
-    #[account(init, payer = user, space = 9000)]                   // how we want to initialize BaseAccount.        // if error Transaction simulation failed: Error processing Instruction 0: custom program error: 0x64 change space = 9000 to space = 10000
+  #[account(init, payer = user, space = 9000)]                   // how we want to initialize BaseAccount.        // if error Transaction simulation failed: Error processing Instruction 0: custom program error: 0x64 change space = 9000 to space = 10000
 //                                                                 // init will tell Solana to create a new account owned by our current program
 //                                                                 // payer = user tells our program who's paying for the account to be created. In this case, it's the user calling the function.
 //                                                                 // allocate 9000 bytes of space for our account
-    pub base_account: Account<'info, BaseAccount>,
-    #[account(mut)]
-    pub user: Signer<'info>,                                       // which is data passed into the program that proves to the program that the user calling this program actually owns their wallet account.
-    pub system_program: Program <'info, System>,                   // id of 11111111111111111111111111111111
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,                                       // which is data passed into the program that proves to the program that the user calling this program actually owns their wallet account.
+  pub system_program: Program <'info, System>,                   // id of 11111111111111111111111111111111
+}
+
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {                                        // Context we set up near the bottom of the program actually becomes useful within the function. It's basically a nice way to say, "Hey, when someone calls add_gif be sure to attach the AddGif context to it as well so the user can access the base_account and whatever else is attached to AddGif.
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
 }
 
 // Tell Solana what we want to store on this account.
 #[account]
 pub struct BaseAccount {                                          // tells our program what kind of account it can make and what to hold inside of it
     pub total_gifs: u64,                                          // Here BaseAccount holds one thing and it's an integer named total_gifs
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
 }
 
 //</3>---------------------------------------------------
--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -46,23 +46,16 @@
 
 //<3>
 const anchor = require('@project-serum/anchor');
-
-// Need the system program, will talk about this soon.
 const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.Myepicproject;
-	
-  // Create an account keypair for our program to use.
-  const baseAccount = anchor.web3.Keypair.generate();                               // need to create some credentials for the BaseAccount we're creating.
-
-  // Call start_stuff_off, pass it the params it needs!
+  const baseAccount = anchor.web3.Keypair.generate();
   let tx = await program.rpc.startStuffOff({
     accounts: {
       baseAccount: baseAccount.publicKey,
@@ -71,12 +64,25 @@ const main = async() => {
     },
     signers: [baseAccount],
   });
-
   console.log("📝 Your transaction signature", tx);
 
-  // Fetch data from the account.
-  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);                   //retrieve the account we created and then access totalGifs
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // You'll need to now pass a GIF link to the function! You'll also need to pass in the user submitting the GIF!
+  await program.rpc.addGif("https://c.tenor.com/hB2WSiEHOE4AAAAd/bhawnao-pe-naa-control-nahi-hain-tumhara-akhandanand-tripathi.gif", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Access gif_list on the account!
+  console.log('👀 GIF List', account.gifList)
 }
 
 const runMain = async () => {
