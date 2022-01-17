358b830b950f0693239f46dcb6366ac24e1ca3f2
abhijitgawai <abhijit.gawai18@vit.edu>
2022-01-17 19:56:35 +0530

    section 2; chapter 4.Retrieve account data.

--- myepicproject/programs/myepicproject/src/lib.rs
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -1,4 +1,4 @@
-//<1>
+//<1>---------------------------------------------------
 // use anchor_lang::prelude::*;
 
 // declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");      // "program id" and has info for Solana on how to run our program. Anchor has generated this
@@ -13,9 +13,29 @@
 
 // #[derive(Accounts)]
 // pub struct Initialize {}
-//</1>
+//</1>---------------------------------------------------
 
-//<2>
+// //<2>---------------------------------------------------
+// use anchor_lang::prelude::*;
+
+// declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+// #[program]
+// pub mod myepicproject {
+//   use super::*;
+//   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+//     Ok(())
+//   }
+// }
+
+// #[derive(Accounts)]
+// pub struct StartStuffOff {}
+// //</2>---------------------------------------------------
+
+
+
+
+//<3>---------------------------------------------------
 use anchor_lang::prelude::*;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -24,12 +44,33 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    // Get a reference to the account.                             // &mut to get a "mutable reference" to base_account  // gives us the power to make changes to base_account. Otherwise, we'd simply be working w/ a "local copy" of base_account.
+    let base_account = &mut ctx.accounts.base_account;             // we just grab base_account from the StartStuffOff context by doing Context<StartStuffOff>.
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
     Ok(())
   }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
-//</2>
+pub struct StartStuffOff<'info> {                                  // here we actually specify how to initialize it and what to hold in our StartStuffOff context
+    #[account(init, payer = user, space = 9000)]                   // how we want to initialize BaseAccount.        // if error Transaction simulation failed: Error processing Instruction 0: custom program error: 0x64 change space = 9000 to space = 10000
+//                                                                 // init will tell Solana to create a new account owned by our current program
+//                                                                 // payer = user tells our program who's paying for the account to be created. In this case, it's the user calling the function.
+//                                                                 // allocate 9000 bytes of space for our account
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,                                       // which is data passed into the program that proves to the program that the user calling this program actually owns their wallet account.
+    pub system_program: Program <'info, System>,                   // id of 11111111111111111111111111111111
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {                                          // tells our program what kind of account it can make and what to hold inside of it
+    pub total_gifs: u64,                                          // Here BaseAccount holds one thing and it's an integer named total_gifs
+}
+
+//</3>---------------------------------------------------
 
 
--- myepicproject/tests/myepicproject.js
+++ myepicproject/tests/myepicproject.js
@@ -16,17 +16,67 @@
 // });
 //</1>
 
-//<2>
+// //<2>
+// const anchor = require('@project-serum/anchor');
+
+// const main = async() => {
+//   console.log("🚀 Starting test...")
+
+//   anchor.setProvider(anchor.Provider.env());
+//   const program = anchor.workspace.Myepicproject;
+//   const tx = await program.rpc.startStuffOff();
+
+//   console.log("📝 Your transaction signature", tx);
+// }
+
+// const runMain = async () => {
+//   try {
+//     await main();
+//     process.exit(0);
+//   } catch (error) {
+//     console.error(error);
+//     process.exit(1);
+//   }
+// };
+
+// runMain();
+
+// //</2>
+
+
+//<3>
 const anchor = require('@project-serum/anchor');
+
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();                               // need to create some credentials for the BaseAccount we're creating.
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);                   //retrieve the account we created and then access totalGifs
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
@@ -41,4 +91,4 @@ const runMain = async () => {
 
 runMain();
 
-//</2>
+//</3>
